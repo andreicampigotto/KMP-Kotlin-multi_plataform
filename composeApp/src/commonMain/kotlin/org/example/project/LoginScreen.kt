eb8a6e20427b76e75d0b4a021668103c3885cf6a
Andrei <andreicampigotto@icloud.com>
2026-01-06 22:28:53 -0500

    login screen

--- composeApp/src/commonMain/kotlin/org/example/project/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/LoginScreen.kt
@@ -1,10 +1,16 @@
 package org.example.project
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,19 +66,48 @@ internal fun LoginScreenContent(
             )
         },
         bottomBar = {
-
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 48.dp)
+            ){
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    content = {
+                        Text(text = "Test")
+                    },
+                    onClick =
+                        onLoginClick,
+                )
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    content = {
+                        Text(text = "Test")
+                    },
+                    onClick = onGoToForgotPassword
+                )
+            }
         },
         snackbarHost ={
 
         },
         content ={
-            Column (modifier = Modifier.fillMaxSize()) {
+            Column (
+                modifier = Modifier.fillMaxSize().padding(horizontal = 24.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
                 TextField(
-                    //modifier = reusableModifierTextField,
+                    modifier = Modifier.fillMaxWidth(),
                     value = uiState.textEmail,
                     isError = uiState.isErrorEmail,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
                     onValueChange = onEmailTextChanged,
+                    label = { "Email here" }
+                )
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = uiState.textPassword,
+                    isError = uiState.isErrorPassword,
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = onPasswordTextChanged,
                     label = { "Email here" }
                 )
             }
